0211e1219d7ee1164aaf77dbf7406547eb06429b
rodrigosilva <rodrigo-cleiton@hotmail.com>
2022-05-18 14:07:47 -0300

    Criando elementos do app

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from '../../components/Header';
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Header name='Samuel Duarte' />
+            <Text>Página Home!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
